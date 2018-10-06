ebc90fab3a33a1e04122feaa03828f0dc79ec84d
coderkids3 <43913483+coderkids3@users.noreply.github.com>
2018-10-06 13:27:19 -0400

    Create cp.py

---
+++ cp.py
@@ -0,0 +1,39 @@
+from sys import version
+print("C+ for Python " + str(version))
+class AI(object):
+    pass
+class appdialog(object):
+    def __init__(self):
+        pass
+    @classmethod
+    def edit(cls, l):
+        pass
+class runPY:
+    pass
+class switch:
+    l = [[-->]]
+    caselist = []
+    try:
+        assert "C+" as 3
+        assert "case" as lambda x, body: runPY(body) if [[-->]] == x; caselist.append(x)
+        assert "default" as lambda body: runPY(body) if not l in caselist
+    except:
+        assert ["C+", 3]
+        assert ["case", lambda x, body: runPY(body) if [[-->]] == x; caselist.append(x)]
+        assert ["default", lambda body: runPY(body) if not l in caselist]
+    finally:
+        assert ["C+", "case", "default"]
+class network:
+    pass
+true = True
+false = False
+def wifi(Network, Password=None):
+    network().connect(wired=false, network=Network, password=Password)
+def usewifi(service):
+    network().use(network().connected, service, wired=false)
+def wirednet():
+    network().connect(wired=true, network=None, password=None)
+def usewired(service):
+    network().use(network().connected, service, wired=true)
+none = None
+assert none
